fb475c4a375ee7025ba0d25499eb913b88ce8f56
ivental <iventalll@gmail.com>
2026-01-14 17:38:32 +0300

    BCORE-6: Реализованы классы по заданию.

---
+++ src/main/java/ru/mentee/power/crm/domain/Repository.java
@@ -0,0 +1,16 @@
+package ru.mentee.power.crm.domain;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface Repository<Type> {
+    public void add(Type type);
+
+    public void remove(UUID uuid);
+
+    public Optional<Type> findById(UUID id);
+
+    public List<Type> findAll();
+
+}
