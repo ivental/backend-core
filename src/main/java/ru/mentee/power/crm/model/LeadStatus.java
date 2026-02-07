53f98b941abe97ea06b07e45077157eb8884a9aa
ivental <iventalll@gmail.com>
2026-02-07 13:37:41 +0300

    GATE-2: доработка таблицы лидов.

--- src/main/java/ru/mentee/power/crm/model/LeadStatus.java
+++ src/main/java/ru/mentee/power/crm/model/LeadStatus.java
@@ -1,7 +1,20 @@
 package ru.mentee.power.crm.model;
 
+
 public enum LeadStatus {
-    NEW,
-    CONTACTED,
-    QUALIFIED
+    NEW("Новый"),
+    CONTACTED("Связались"),
+    QUALIFIED("Квалифицирован"),
+    DONE("Готово");
+
+
+    private final String russianName;
+
+    LeadStatus(String russianName) {
+        this.russianName = russianName;
+    }
+
+    public String getRussianName() {
+        return russianName;
+    }
 }
