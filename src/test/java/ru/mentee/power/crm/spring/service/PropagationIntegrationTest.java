de103e7dcbe0f5021219914d767d3cb50c9c8463
ivental <iventalll@gmail.com>
2026-02-24 14:05:45 +0300

    Исправление Actions

--- src/test/java/ru/mentee/power/crm/spring/service/PropagationIntegrationTest.java
+++ src/test/java/ru/mentee/power/crm/spring/service/PropagationIntegrationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 import ru.mentee.power.crm.spring.model.Lead;
 import ru.mentee.power.crm.spring.model.LeadStatusJpa;
@@ -15,6 +16,7 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class PropagationIntegrationTest {
     @Autowired
     private PropagationDemoService demoService;
